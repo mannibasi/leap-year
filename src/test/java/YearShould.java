07724e6a46e85060e2813d1ea8e304fec9487e3f
= <manni.basi@gmail.com>
2023-08-24 21:47:57 +0100

    Added new failing test

--- src/test/java/YearShould.java
+++ src/test/java/YearShould.java
@@ -2,11 +2,18 @@ import org.junit.jupiter.api.Test;
 import org.katas.Year;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class YearShould {
     @Test
     public void return_false_if_not_divisible_by_4() {
         Year year = new Year(1997);
         assertFalse(year.isLeap());
+    }
+
+    @Test
+    public void return_true_if_divisible_by_4() {
+        Year year = new Year(1996);
+        assertTrue(year.isLeap());
     }
 }
